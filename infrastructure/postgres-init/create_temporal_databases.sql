17f2193b652f7bd6d27c9786ca1fb2181deb3348
bilal68 <bilalamin68@gmail.com>
2026-01-21 13:31:47 +0500

    temporal issue fixed on dev

---
+++ infrastructure/postgres-init/create_temporal_databases.sql
@@ -0,0 +1,7 @@
+-- extra app DBs (
+-- CREATE DATABASE smartcourse_analytics;
+-- CREATE DATABASE smartcourse_ai;
+
+-- Temporal required DBs
+CREATE DATABASE temporal;
+CREATE DATABASE temporal_visibility;
